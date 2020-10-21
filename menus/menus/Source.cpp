addd596e346771c9cd1582d0bbd2a2d6f44130f8
NataBowl <natashabowles01@gmail.com>
2020-10-21 14:46:01 -0400

    menu

---
+++ menus/menus/Source.cpp
@@ -0,0 +1,47 @@
+//this part is going to display the different menus and options of Townies and Terrors
+#include <iostream>
+#include <string>
+using namespace std;
+
+// declares functions for the game and info
+
+
+void story()
+{
+	cout << "This is where story goes.";
+}
+
+int main() 
+{
+	std::string ask;
+	int hold = 1;
+	cout << "Townies and Terrors" << endl << endl;
+
+	//asks users where they would like to go
+	cout << "Please enter where you would like to go" << endl;
+	cout << "play" << endl << endl << "info" << endl << endl << "quit" << endl;
+	cin >> ask;
+
+	while (hold != 0)
+	{
+		if (ask == "play")
+		{
+			hold = 0;
+		}
+		else if (ask == "info")
+		{
+			story ();
+			hold = 0;
+		}
+		else if (ask == "quit")
+		{
+			hold = 0;
+			exit;
+		}
+		else
+		{
+			cout << "Please enter one of the options";
+			cin >> ask;
+		}
+	}
+}
