fa8bbca5724e9f80e15169430986d79d9d1d7758
NataBowl <natashabowles01@gmail.com>
2020-10-22 21:07:40 -0400

    Everything

--- cardAssignmentTest/main.cpp
+++ cardAssignmentTest/main.cpp
@@ -2,6 +2,247 @@
 #include <ctime>
 #include <conio.h>
 
+
+
+
+/*//this part is going to display the different menus and options of Townies and Terrors
+#include <string>
+using namespace std;
+
+// declares functions for the game and info
+
+
+void story()
+{
+	int peoples;
+	int type;
+	int persons;
+restartone:
+	cout << "Which would you like to see?" << endl << endl << "1: townies" << endl << endl << "2: terrors?" << endl;
+
+	peoples = _getch();
+
+	if (peoples == 50)
+	{
+	restarttwo:
+		cout << "\n\nWhich type of Terror would you like to learn about?\n1: Power\n2: Speed\n3: Support\n\n";
+		type = _getch();
+
+		if (type == 49)
+		{
+		restartthree:
+			cout << "\n\nWhich Terror would you like to learn about?\n1: Amalgam\n2: Wolfman\n3: Zombie\n\n";
+			persons = _getch(); //lets see
+
+			if (persons == 49)
+			{
+				cout << "\n\nA creature made from the body parts of various corpses brought to life. He now seeks vengeance on the man that created him and the humans that hate him.\n\nAbility: Crash of Hatred\nHP: 4\nStrength: 5\n\n";
+			}
+			else if (persons == 50)
+			{
+				cout << "\n\nA being cursed to take the form of the wolf. He lacks control and can only feel the bloodlust pumping through his veins.\n\nAbility: Wolf Crash\nHP: 3\nStrength : 4\n\n";
+			}
+			else if (persons == 51)
+			{
+				cout << "\n\nA mindless corpse, brought back to life by the magic of the night. It only wishes to feed on the flesh of the living.\n\nAbility: Mindless Crash\nHP: 2\nStrength: 3\n\n";
+			}
+			else
+			{
+				cout << "That was not a correct choice.\n\n";
+				goto restartthree;
+			}
+		}
+
+		else if (type == 50)
+		{
+		restartfour:
+			std::cout << "\n\nWhich terror would you like to learn about?\n1: Vampire\n2: Ghost\n3: Shifter\n\n";
+			persons = _getch();
+
+			if (persons == 49)
+			{
+				std::cout << "\n\nThe original creature of the night. A thousand year old woman looking to drink the blood of anyone she can find.\n\nAbility: Bat Dash\nHP: 4\nStrength: 4\n\n";
+			}
+			else if (persons == 50)
+			{
+				std::cout << "\n\nA vengeful student looking to kill as many humans as he can, even if he has to rely on the power of others to do so.\n\nAbility: Spiritual Dash\nHP: 2\nStrength: 2\n\n";
+			}
+			else if (persons == 51)
+			{
+				std::cout << "\n\nA doctor who used his knowledge to allow himself to change shape. Tonight, he is looking to have some fun.\n\nAbility: Changing Dash\nHP: 3\nStrength: 3\n\n";
+			}
+			else
+			{
+				cout << "That was not a correct choice.\n\n";
+				goto restartfour;
+			}
+		}
+
+		else if (type == 51)
+		{
+		restartfive:
+			std::cout << "\n\nWhich Terror would you like to learn about?\n1: Reaper\n2: Banshee\n3: Witch\n\n";
+			persons = _getch();
+
+			if (persons == 49)
+			{
+				std::cout << "\n\nThe personification of death. He has heard the wails and felt the need for vengeance. He has come to reap its aftermath.\n\nAbility: Deadly Stash\nHP: 5\nStrength: 4\n\n";
+			}
+			else if (persons == 50)
+			{
+				std::cout << "\n\nA long dead woman who comes to wail outside the window of those soon to be deceased. Tonight, she has started to wail all around town.\n\nAbility: Wail of Stash\nHP: 3\nStrength: 2\n\n";
+			}
+			else if (persons == 51)
+			{
+				std::cout << "\n\nAn outcast who is using her magic to help the wicked monsters.\n\nAbility: Magic Stash\nHP: 4\nStrength: 3\n\n";
+			}
+			else
+			{
+				cout << "That was not a correct choice.\n\n";
+				goto restartfive;
+			}
+		}
+		else
+		{
+			cout << "That was not a correct choice.\n\n";
+			goto restarttwo;
+		}
+	}
+	else if (peoples == 49)
+	{
+	restartsix:
+		std::cout << "\n\nWhich type of Townie would you like to learn about?\n1: Power\n2: Speed\n3: Support\n\n";
+		type = _getch();
+
+		if (type == 49)
+		{
+		restartseven:
+			std::cout << "\n\nWhich Townie would you like to learn about?\n1: Detective\n2: Lover\n3: Noble\n\n";
+			persons = _getch();
+
+			if (persons == 49)
+			{
+				std::cout << "\n\nA woman trying to solve a string of mysterious murders, leading to even more mysterious beings.\n\nAbility: Lawful Crash\nHP: 4\nStrength: 5\n\n";
+			}
+			else if (persons == 50)
+			{
+				std::cout << "\n\nShe is willing to do anythnig for her love. Even if it means fighting monsters of the night.\n\nAbility: Crash of Love.\nHP: 3\nStrength : 4\n\n";
+			}
+			else if (persons == 51)
+			{
+				std::cout << "\n\nThe nobleman who looks after the town. After a string of murders, he is forced to face the possibility of something sinister lurking.\n\nAbility: Noble's Mighty Crash\nHP: 2\nStrength: 3\n\n";
+			}
+			else
+			{
+				cout << "That was not a correct choice.\n\n";
+				goto restartseven;
+			}
+		}
+		else if (type == 50)
+		{
+		restarteight:
+			std::cout << "\n\nWhich Townie would you like to learn about?\n1: Beauty\n2: Dancer\n3: Student\n\n";
+			persons = _getch();
+
+			if (persons == 49)
+			{
+				std::cout << "\n\nThe most beautiful woman in town. She stuns everyone with her charm. Even the monsters who wish to feast on her blood.\n\nAbility: Stunning Dash\nHP: 4\nStrength: 4\n\n";
+			}
+			else if (persons == 50)
+			{
+				std::cout << "\n\nA young woman who dances at night for the pleasure of many. Her nightly outings have brought her to the attention of the other creatures of the night.\n\nAbility: Dashing Charm\nHP: 2\nStrength: 2\n\n";
+			}
+			else if (persons == 51)
+			{
+				std::cout << "\n\nA young boy just looking to learn. Now, monsters are after him and he must run.\n\nAbility: Youthful Dash\nHP: 3\nStrength: 3\n\n";
+			}
+			else
+			{
+				cout << "That was not a correct choice.\n\n";
+				goto restarteight;
+			}
+		}
+		else if (type == 51)
+		{
+		restartnine:
+			std::cout << "\n\nWhich Townie would you like to learn about?\n1: Scientist\n2: Mentor\n3: Doctor\n\n";
+			persons = _getch();
+
+			if (persons == 49)
+			{
+				std::cout << "\n\nA man who has spent his whole life trying to solve the mysteries of the world. He knows the danger that is coming. Coming for him.\n\nAbility: Stash of Knowledge\nHP: 5\nStrength: 4\n\n";
+			}
+			else if (persons == 50)
+			{
+				std::cout << "\n\nA older man who teaches the young. He is just an unlucky bystander to the monster's night attack.\n\nAbility: Wiseman's Stash\nHP: 3\nStrength: 2\n\n";
+			}
+			else if (persons == 51)
+			{
+				std::cout << "\n\nAn old man who has seen a night like this before. He just wishes to lessen the number of casualties and survive the night.\n\nAbility: Blood Stash\nHP: 4\nStrength: 3\n\n";
+			}
+			else
+			{
+				cout << "That was not a correct choice.\n\n";
+				goto restartnine;
+			}
+		}
+		else
+		{
+			cout << "That was not a correct choice.\n\n";
+			goto restartsix;
+		}
+	}
+	else
+	{
+		cout << "That was not a correct choice.\n\n";
+		goto restartone;
+	}
+
+	persons = _getch();
+	//cout << people;
+}
+
+int main()
+{
+	std::string ask;
+	int hold = 1;
+	cout << "Townies and Terrors" << endl << endl;
+
+	//asks users where they would like to go
+	cout << "Please enter where you would like to go" << endl;
+	cout << "play" << endl << endl << "info" << endl << endl << "quit" << endl;
+	cin >> ask;
+
+	while (hold != 0)
+	{
+		if (ask == "play")
+		{
+			hold = 0;
+		}
+		else if (ask == "info")
+		{
+			story();
+			hold = 0;
+		}
+		else if (ask == "quit")
+		{
+			hold = 0;
+			exit;
+		}
+		else
+		{
+			cout << "Please enter one of the options";
+			cin >> ask;
+		}
+	}
+}
+//hehehehee sneaky comment
+*/
+
+
+
+
+
 //
 //bool specialPoint = true;
 //bool strengthBoost = false;
@@ -539,4 +780,112 @@ int main()
 }
 
 
+
+
+
+
+/*// CombatSystem.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <time.h>
+#include <cstdlib>
+#include <windows.h>
+int rollDice();//function designed to simulate a dice roll
+void battle(int& card1Health, int& card1Strength, int& card2Health, int& card2Strength);//function designed to simulate a battle between two cards
+void SetColor(int);
+int main()
+{
+	int doctorHealth = 4;//test case
+	int doctorStrength = 3;//test case
+
+	int witchHealth = 4;//test case
+	int witchStrength = 3;//test case
+
+	battle(doctorHealth, doctorStrength, witchHealth, witchStrength);//function is called here
+
+}
+int rollDice()
+{
+	int diceFacingUp;//elaborate variable name to avoid plagiarism
+	srand(time(0)+rand());//not exactly sure what's goingg on here but it works.
+
+	diceFacingUp = rand() % 7;//ensures that the random number is between 0 and 6
+	while (diceFacingUp == 0)//if the value of diceFacingUp is 0, it will be rerolled until it is not zero
+	{
+		diceFacingUp = rand() % 7;
+	}
+	return diceFacingUp;
+}
+
+void SetColor(int value)
+{
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), value);
+}
+
+void battle(int& card1Health, int& card1Strength, int& card2Health, int& card2Strength)
+{
+	//card1 represents the cards of the player who initiated the battle
+	//card2 represents the cards of the player who is being attacked
+	//the stats of the player who initiated the battle should always be written in the first 2 parameters(CONTINUED)
+	//while the stats of the player being attacked should be written in the last 2 parameters
+	bool p1Attacking = true;//used to decide which player is rolling the dice.
+	bool p2Attacking = false;//used to decide which player is rolling the dice.
+	bool battleEnd = false;
+	int diceRoll;
+	while (battleEnd == false)
+	{
+		if (p1Attacking == true)
+		{
+			SetColor(11);
+			diceRoll = rollDice();
+			std::cout << "Player 1's Turn\n";
+			std::cout << "Number rolled: " << diceRoll << "\n";
+			if (diceRoll >= card2Strength)//checks to see if the number rolled by the player is greater or equal to the opposing card's strength
+			{
+				card2Health -= 1;
+				std::cout << "Card1 deals damage to Card2\n";
+				std::cout << "Card1 health: " << card1Health << " Card1 Strength: " << card1Strength << "\n";
+				std::cout << "Card2 health: " << card2Health << " Card2 Strength: " << card2Strength << "\n";
+			}
+			else
+			{
+				std::cout << "No damage done\n";
+			}
+			p1Attacking = false;
+			p2Attacking = true;
+		}
+		else if (p2Attacking == true)
+		{
+			SetColor(5);
+			diceRoll = rollDice();
+			std::cout << "Player 2's Turn\n";
+			std::cout << "Number rolled: " << diceRoll << "\n";
+			if (diceRoll >= card1Strength)
+			{
+				card1Health -= 1;
+				std::cout << "Card2 deals damage to Card1\n";
+				std::cout << "Card1 health: " << card1Health << " Card1 Strength: " << card1Strength << "\n";
+				std::cout << "Card2 health: " << card2Health << " Card2 Strength: " << card2Strength << "\n";
+			}
+			else
+			{
+				std::cout << "No damage done\n";
+			}
+			p1Attacking = true;
+			p2Attacking = false;
+		}
+		if (card1Health <= 0)
+		{
+			std::cout << "Player2 wins\n";
+			battleEnd = true;
+		}
+		if (card2Health <= 0)
+		{
+			std::cout << "Player1 wins\n";
+			battleEnd = true;
+		}
+	}
+}
+*/
 
